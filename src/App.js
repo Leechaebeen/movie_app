734774002747d6f8ee7c979152cae84814bcbd81
tech7billie <tech.billie@gmail.com>
2021-03-05 14:04:24 +0900

    Fetching Movies From API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 // react 는 자동으로 모든 class component 의 render() 를 실행한다.
 // render() 는 React.Component 의 메소드를 오버라이딩 하는 것 
@@ -11,16 +12,16 @@ class App extends React.Component
     movies : []
   }
 
+  // 비동기로 json 데이터 가져오는 메소드 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+  }
+
   // 렌더링이 되면 가장 먼저 호출되는 메소드
   componentDidMount()
   {
-    setTimeout (()=>{
-      this.setState({isLoading : false})
-    }, 6000);
-  
-    axios
+    this.getMovies();
   }
-
 
   // 렌더링 
   render()
