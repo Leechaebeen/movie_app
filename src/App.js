d989d1f0f9e90480bd7f993c8e5b026b8a772ca9
tech7billie <tech.billie@gmail.com>
2021-03-05 10:53:30 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Potato from "./Potato";
-
 // component : HTML 을 반환하는 함수 
-function App() {
+// jsx : js 안의 HTML. 컴포넌트를 만들고 어떻게 사용하는지에 대한 것  
+
+function Food({fav})
+{
+  // props.fav 와 {fav} 는 같은 구문이다.
+  // console.log(props.fav);
+  return <h1>I like {fav}</h1>
+}
+
+function App() 
+{
   return (
     <div className="App">
       HELLO!!!
-      <Potato/>
+      {/* Food 컴포넌트의 fav 라는 속성에 kimchi 라는 값 부여. 이 값들은 food 컴포넌트에 전달된다. */}
+      <Food fav="kimchi"/>
+      <Food fav="ramyeon"/>
+      <Food fav="samgyopsal"/>
+      <Food fav="zzuggumi"/>
     </div>
   );
 }
