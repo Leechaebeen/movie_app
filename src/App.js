5bf4c8d2d635b67c1eab1f40da8be9b59bd64b66
tech7billie <tech.billie@gmail.com>
2021-03-05 13:26:12 +0900

    know aout state

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ class App extends React.Component
   };
 
   add = () => {
-    console.log("add");
+    // setState() 를 호출하면 react 는 state 를 refresh 하고 >> render() 메소드를 호출 << 한다.
+    // current 를 통해서 현재 state 를 얻을 수 있다.
+    this.setState( current => ({ count : current.count + 1 }));
   };
 
   minus = () => {
-    console.log("minus");
+    // 이런 방식은 추천하지 않는다.
+    this.setState({ count: this.state.count - 1 });
   };
   
 
