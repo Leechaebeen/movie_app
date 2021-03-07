56017ed5ffaaa000fd2948977209af519e7e1bcc
billie <tech.billie@gmail.com>
2021-03-07 22:42:58 +0900

    Building the Router : exact 속성을 사용해서 특정 경로인 경우에만 컴포넌트 렌더링하도록 지정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
-import Home from "./routes/Ho"
+import Home from "./routes/Home";
 
 // 'path 속성 경로를 요청하면 이 component 를 보여줘' 라고 지정 
 function App()
 {
   return ( 
   <HashRouter>  
-    <Route path="/" component={Home}></Route>
-    <Route path="/about" component={About } ></Route>
+    <Route path="/" exact={true} component={Home}/>
+    <Route path="/about" component={About}></Route>
   </HashRouter>
   );
 }
