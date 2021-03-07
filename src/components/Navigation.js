62b9646f35c03b75c19061322cd6a09558bf4d53
billie <tech.billie@gmail.com>
2021-03-07 23:02:58 +0900

    Building the Navigation - 이렇게 하면 페이지를 새로고침힌다.

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import App from "../App";
+
+function Navigation()
+{
+    return (<div>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+    </div>);
+}
+
+export default Navigation;
